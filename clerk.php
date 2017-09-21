e7a3ae575a23d83bef2924bc63652a30a34a54e8
Rasmus Bech <me@rbech.com>
2017-09-21 15:51:30 +0200

    Bump version

--- clerk.php
+++ clerk.php
@@ -3,7 +3,7 @@
  * Plugin Name: Clerk
  * Plugin URI: https://clerk.io/
  * Description: Clerk.io Turns More Browsers Into Buyers
- * Version: 1.3.0
+ * Version: 1.3.1
  * Author: Clerk.io
  * Author URI: https://clerk.io
  *
--- readme.txt
+++ readme.txt
@@ -7,6 +7,9 @@ WC requires at least: 2.6
 WC tested up to: 3.0
 
 == Changelog ==
+= 1.3.1 - 2017-09-20 =
+* Fix error with get_status in WooCommerce 2.6
+
 = 1.3.0 - 2017-09-20 =
 * Add insights dashboards
 
