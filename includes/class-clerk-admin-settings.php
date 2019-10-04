0d8f9efdaecdb04ef2781a1125aa3a5aec6da011
Casper Kromann Nielsen <ckn@clerk.io>
2019-10-04 11:11:32 +0200

    1.7.0 to 1.7.1

--- includes/class-clerk-admin-settings.php
+++ includes/class-clerk-admin-settings.php
@@ -20,7 +20,7 @@ class Clerk_Admin_Settings
         $this->initHooks();
         require_once(__DIR__ . '/class-clerk-logger.php');
         $this->logger = new ClerkLogger();
-        $this->version = '1.7.0';
+        $this->version = '1.7.1';
 
         $this->InitializeSettings();
 
