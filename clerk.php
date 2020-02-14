408970a11b74448eac4498cd519d3239cda5bf3a
Casper Kromann Nielsen <ckn@clerk.io>
2020-02-14 08:36:58 +0100

    Turning version number

--- clerk.php
+++ clerk.php
@@ -3,7 +3,7 @@
  * Plugin Name: Clerk
  * Plugin URI: https://clerk.io/
  * Description: Clerk.io Turns More Browsers Into Buyers
- * Version: 2.2.5
+ * Version: 2.2.6
  * Author: Clerk.io
  * Author URI: https://clerk.io
  *
--- includes/class-clerk-admin-settings.php
+++ includes/class-clerk-admin-settings.php
@@ -20,7 +20,7 @@ class Clerk_Admin_Settings
         $this->initHooks();
         require_once(__DIR__ . '/class-clerk-logger.php');
         $this->logger = new ClerkLogger();
-        $this->version = '2.2.5';
+        $this->version = '2.2.6';
 
         $this->InitializeSettings();
 
