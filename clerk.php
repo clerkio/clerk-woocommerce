e86ce6ef05245b2a4b7a588dac9e9fb813ce754a
Wissam <wissam.maarouf@live.dk>
2022-03-18 00:52:16 +0100

    Fix Clerk to the right version 3.6.1

--- clerk.php
+++ clerk.php
@@ -3,7 +3,7 @@
  * Plugin Name: Clerk
  * Plugin URI: https://clerk.io/
  * Description: Clerk.io Turns More Browsers Into Buyers
- * Version: 3.5.2
+ * Version: 3.6.1
  * Author: Clerk.io
  * Author URI: https://clerk.io
  *
