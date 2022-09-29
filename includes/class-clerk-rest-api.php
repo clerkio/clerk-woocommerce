0dbcf1b9570813f8f4cef7c99c22748ad82f2ba9
Alexander Stage <89036433+FoulBachelor@users.noreply.github.com>
2022-09-29 11:10:58 +0200

    Fixed variant price empty array

--- includes/class-clerk-rest-api.php
+++ includes/class-clerk-rest-api.php
@@ -280,7 +280,7 @@ class Clerk_Rest_Api extends WP_REST_Server
                         $regularPrice[$variant_id] = $variation['display_regular_price'];
                     }
 
-                    if(!empty($lowestDisplayPrice)){
+                    if(!empty($displayPrice)){
                         $lowestDisplayPrice = array_keys($displayPrice, min($displayPrice)); // Find the corresponding product ID
                         $price = $displayPrice[$lowestDisplayPrice[0]]; // Get the lowest price
                         $list_price = $regularPrice[$lowestDisplayPrice[0]]; // Get the corresponding list price (regular price)
