76ca22a596bec245d5401cee426d7051cf391ec0
Foulbachelor <abs@clerk.io>
2022-11-15 23:26:38 +0100

    wp standards update

--- templates/clerk-related-products.php
+++ templates/clerk-related-products.php
@@ -1,4 +1,19 @@
 <?php
+/**
+ * Plugin Name: Clerk
+ * Plugin URI: https://clerk.io/
+ * Description: Clerk.io Turns More Browsers Into Buyers
+ * Version: 3.8.3
+ * Author: Clerk.io
+ * Author URI: https://clerk.io
+ *
+ * Text Domain: clerk
+ * Domain Path: /i18n/languages/
+ * License: MIT
+ *
+ * @package clerkio/clerk-woocommerce
+ */
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
@@ -6,24 +21,34 @@ if ( ! defined( 'ABSPATH' ) ) {
 $options = get_option( 'clerk_options' );
 
 if ( isset( $options['product_enabled'] ) && $options['product_enabled'] ) :
-    $contents = explode(',', $options['product_content']);
-    $index = 0;
-    $class_string = 'clerk_';
-    $filter_string = '';
-    $unique_filter = (isset($options['product_excl_duplicates']) && $options['product_excl_duplicates']) ? true : false;
-    foreach ($contents as $content) :
+	$contents      = explode( ',', $options['product_content'] );
+	$index         = 0;
+	$class_string  = 'clerk_';
+	$filter_string = '';
+	$unique_filter = ( isset( $options['product_excl_duplicates'] ) && $options['product_excl_duplicates'] ) ? true : false;
+	foreach ( $contents as $content ) :
 
-        ?>
-        <span class="clerk <?php if($unique_filter){ echo $class_string.(string)$index; } ?>" 
-            <?php if($index > 0 && $unique_filter){ echo 'data-exclude-from="'.$filter_string.'"'; }?>
-            data-template="@<?php echo str_replace(' ','', $content); ?>"
-            data-products="[<?php echo get_the_ID(); ?>]"></span>
-        <?php
-        if($index > 0){
-            $filter_string .= ', ';
-        }
-        $filter_string .= '.'.$class_string.(string)$index;
-        $index++;
-    endforeach;
+		?>
+		<span class="clerk
+		<?php
+		if ( $unique_filter ) {
+			echo esc_attr( $class_string . (string) $index );
+		}
+		?>
+		"
+		<?php
+		if ( $index > 0 && $unique_filter ) {
+			echo 'data-exclude-from="' . esc_attr( $filter_string ) . '"';
+		}
+		?>
+			data-template="@<?php echo esc_attr( str_replace( ' ', '', $content ) ); ?>"
+			data-products="[<?php echo get_the_ID(); ?>]"></span>
+		<?php
+		if ( $index > 0 ) {
+			$filter_string .= ', ';
+		}
+		$filter_string .= '.' . $class_string . (string) $index;
+		$index++;
+	endforeach;
 endif;
 ?>
