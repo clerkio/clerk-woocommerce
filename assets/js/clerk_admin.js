650fd892cad86e14f2cacfbbdcebd220706c18a7
Foulbachelor <abs@clerk.io>
2022-11-15 23:44:01 +0100

    wp standards update

--- assets/js/clerk_admin.js
+++ assets/js/clerk_admin.js
@@ -1,55 +1,78 @@
-jQuery(document).ready(function ($) {
+/**
+ * Plugin Name: Clerk
+ * Plugin URI: https://clerk.io/
+ * Description: Clerk.io Turns More Browsers Into Buyers
+ * Version: 3.8.3
+ * Author: Clerk.io
+ * Author URI: https://clerk.io
+ *
+ * Text Domain: clerk
+ * Domain Path: /i18n/languages/
+ * License: MIT
+ *
+ * @package clerkio/clerk-woocommerce
+ */
 
-    var before_logging_level;
+jQuery( document ).ready(
+	function ($) {
 
-    $('#clerk-dialog').dialog({
-        title: 'Changing Logging Level',
-        dialogClass: 'wp-dialog',
-        autoOpen: false,
-        draggable: false,
-        width: 'auto',
-        modal: true,
-        resizable: false,
-        closeOnEscape: false,
-        position: {
-            my: "center",
-            at: "center",
-            of: window
-        },
-        buttons: {
-            "Cancel": function () {
-                document.querySelector('#log_level [value="' + before_logging_level + '"]').selected = true;
-                $(this).dialog('close');
-            },
-            "I'm Sure": function () {
-                $(this).dialog('close');
-            },
+		var before_logging_level;
 
-        },
-        open: function () {
-            // close dialog by clicking the overlay behind it
-            $(".ui-dialog-titlebar-close").hide();
+		$( '#clerk-dialog' ).dialog(
+			{
+				title: 'Changing Logging Level',
+				dialogClass: 'wp-dialog',
+				autoOpen: false,
+				draggable: false,
+				width: 'auto',
+				modal: true,
+				resizable: false,
+				closeOnEscape: false,
+				position: {
+					my: "center",
+					at: "center",
+					of: window
+				},
+				buttons: {
+					"Cancel": function () {
+						document.querySelector( '#log_level [value="' + before_logging_level + '"]' ).selected = true;
+						$( this ).dialog( 'close' );
+					},
+					"I'm Sure": function () {
+						$( this ).dialog( 'close' );
+					},
 
-        },
-    });
+				},
+				open: function () {
+					// close dialog by clicking the overlay behind it.
+					$( ".ui-dialog-titlebar-close" ).hide();
 
-    $('#clerk-dialog').html('<p>Debug Mode should not be used in production! Are you sure you want to change logging level to Debug Mode ? </p>');
+				},
+			}
+		);
 
-    $('#log_level').focus(function () {
+		$( '#clerk-dialog' ).html( '<p>Debug Mode should not be used in production! Are you sure you want to change logging level to Debug Mode ? </p>' );
 
-        before_logging_level = $('#log_level').val();
+		$( '#log_level' ).focus(
+			function () {
 
-    }).change(function () {
+				before_logging_level = $( '#log_level' ).val();
 
-        if ($('#log_level').val() !== 'Error + Warn + Debug Mode') {
+			}
+		).change(
+			function () {
 
-            before_logging_level = $('#log_level').val();
+				if ($( '#log_level' ).val() !== 'Error + Warn + Debug Mode') {
 
-        } else {
+					before_logging_level = $( '#log_level' ).val();
 
-            $('#clerk-dialog').dialog('open');
+				} else {
 
-        }
+					$( '#clerk-dialog' ).dialog( 'open' );
 
-    });
-});
+				}
+
+			}
+		);
+	}
+);
