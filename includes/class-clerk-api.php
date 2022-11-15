ba6734f9e4eb3cd521461d4a00af07739d97406f
Foulbachelor <abs@clerk.io>
2022-11-15 18:20:27 +0100

    fixed array

--- includes/class-clerk-api.php
+++ includes/class-clerk-api.php
@@ -62,7 +62,7 @@ class Clerk_Api {
 			$params = array(
 				'key'         => $options['public_key'],
 				'private_key' => $options['private_key'],
-				'products'    => $product_id . ',',
+				'products'    => array( $product_id ),
 			);
 
 			$this->get( 'product/remove', $params );
