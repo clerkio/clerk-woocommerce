e69638990a7c181e89c701a0f8121f474257bd94
Stubbe <abs@clerk.io>
2023-08-01 11:13:55 +0200

    bumped version number to 4.1.0

--- includes/widgets/class-clerk-widget-search.php
+++ includes/widgets/class-clerk-widget-search.php
@@ -3,7 +3,7 @@
  * Plugin Name: Clerk
  * Plugin URI: https://clerk.io/
  * Description: Clerk.io Turns More Browsers Into Buyers
- * Version: 4.0.9
+ * Version: 4.1.0
  * Author: Clerk.io
  * Author URI: https://clerk.io
  *
