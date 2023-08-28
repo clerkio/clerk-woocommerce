6b9dab9f4328bedc72f4ebff27b20c2a3eddbbf8
LGXerxes <ditisluca@yahoo.it>
2023-08-28 10:52:53 +0200

    removed empty check

--- includes/class-clerk-rest-api.php
+++ includes/class-clerk-rest-api.php
@@ -1167,7 +1167,7 @@ class Clerk_Rest_Api extends WP_REST_Server {
 				$user_meta = get_user_meta($user->ID);
 
 				foreach ($customer_additional_fields as $customer_additional_field) {
-					if (isset($user_meta[$customer_additional_field]) && !empty($user_meta[$customer_additional_field])) {
+					if (isset($user_meta[$customer_additional_field])) {
 						$_customer[$customer_additional_field] = $user_meta[$customer_additional_field][0];
 					}
 				}
