5842b7d769fd031fdb152fa1bb9edf7af22a2ae0
Luca Giannini <68999840+LGXerxes@users.noreply.github.com>
2023-10-19 15:38:26 +0200

    wp_enqueue_script without jquery for powerstep.js (#75)

--- includes/class-clerk-powerstep.php
+++ includes/class-clerk-powerstep.php
@@ -279,7 +279,7 @@ class Clerk_Powerstep {
 				wp_enqueue_style( 'clerk_powerstep_css', plugins_url( '../assets/css/powerstep.css', __FILE__ ), array(), get_bloginfo( 'version' ) );
 			}
 
-			wp_enqueue_script( 'clerk_powerstep_js', plugins_url( '../assets/js/powerstep.js', __FILE__ ), array( 'jquery' ), get_bloginfo( 'version' ), true );
+			wp_enqueue_script( 'clerk_powerstep_js', plugins_url( '../assets/js/powerstep.js', __FILE__ ), array(), get_bloginfo( 'version' ), true );
 			wp_localize_script(
 				'clerk_powerstep_js',
 				'variables',
