0b95135666a443cd36bd2dba48ff9f4540bf624b
Stubbe <abs@clerk.io>
2024-01-16 09:20:34 +0100

    prevent sync url overwrite

--- includes/class-clerk-admin-settings.php
+++ includes/class-clerk-admin-settings.php
@@ -2237,8 +2237,8 @@ class Clerk_Admin_Settings
             </h1>
 
             <form id="clerkAdminForm" action="options.php" method="post">
-                <div id="multi-lang-data"><?php echo esc_html($language_info); ?></div>
-                <div id="hidden-lang-data" class="clerk_hidden" data-is-pll="<?php echo $is_pll; ?>"><?php echo esc_html($lang_info_json); ?></div>
+                <div id="multi-lang-data" class="clerk_hidden" data-is-pll="<?php echo $is_pll; ?>"><?php echo esc_html($language_info); ?></div>
+                <div id="hidden-lang-data" class="clerk_hidden" ><?php echo esc_html($lang_info_json); ?></div>
                 <?php
                 // output security fields for the registered setting "wporg".
                 settings_fields('clerk');
