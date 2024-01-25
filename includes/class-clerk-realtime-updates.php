2e81974fa1c8e908207ae8001a40a8b188d7d41a
Gary Kealy <hello@garykealy.dev>
2024-01-25 13:04:58 +0100

    fix: php error call to member function on bool

--- includes/class-clerk-realtime-updates.php
+++ includes/class-clerk-realtime-updates.php
@@ -518,6 +518,9 @@ class Clerk_Product_Sync {
 						$child_ids           = $product->get_children();
 						foreach ( $child_ids as $key => $value ) {
 							$child                 = wc_get_product( $value );
+							if ( empty( $child ) ) {
+								continue;
+							}
 							$tmp_children_prices[] = $child->get_regular_price();
 						}
 						if ( ! empty( $tmp_children_prices ) ) {
