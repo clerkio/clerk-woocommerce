60993ac2e8510c399a53f517eb8ee66608945c4b
mah-work <132890999+mah-work@users.noreply.github.com>
2024-10-21 20:38:42 +0200

    Sorry

--- includes/class-clerk-admin-settings.php
+++ includes/class-clerk-admin-settings.php
@@ -114,12 +114,12 @@ class Clerk_Admin_Settings {
 		$options = clerk_get_options();
 
 		// Add general section.
-		add_settings_section{
+		add_settings_section(
 			'clerk_testing_info',
 			__('Testing', 'clerk'),
 			null,
 			'clerk'
-		};
+		);
 
 		add_settings_section(
 			'clerk_section_general',
